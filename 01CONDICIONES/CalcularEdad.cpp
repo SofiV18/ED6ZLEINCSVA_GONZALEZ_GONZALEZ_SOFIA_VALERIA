ab48bdd0be65ec83a73a4cc6e6c1c8863c776871
sv.gonzalez.g@edu.utc.mx <sv.gonzalez.g@edu.utc.mx>
2022-05-25 12:32:52 -0500

    EjemploCondicional

---
+++ 01CONDICIONES/CalcularEdad.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+ 
+void caEdad( int Aa , int Ma, int Da, int An, int Mn, int Dn);
+ 
+int main ()
+{
+    int Aa, Ma, Da;
+    int An, Mn, Dn; 
+
+    cout<<"CALCULAR EDAD"<<endl;
+    cout<<endl<<"Año Actual: "<<endl;
+	cin>>Aa;
+    cout<<"Mes Actual: "<<endl;  
+	cin>>Ma;
+    cout<<"Dia Actual: "<<endl;
+	cin>>Da;
+	
+ 
+    cout<<endl<<"Año Nacimiento: "<<endl; 
+	cin>>An;
+    cout<<"Mes de Nacimiento: "<<endl;
+	cin>>Mn;
+    cout<<"Dia de Nacimiento: "<<endl;
+	cin>>Dn;
+ 
+    caEdad( Aa , Ma, Da, An, Mn, Dn);
+ 
+    return 0;
+}
+ 
+
+void caEdad( int Aa , int Ma, int Da, int An, int Mn, int Dn)
+{
+    int rF , rM, Fa, Fn;
+ 
+    if (Fn<Fa)
+    { 
+        Fa=Fa+30; 
+        Ma=Ma-1; 
+        rF=Fa-Fn; 
+    }
+    else 
+        rF=Fa-Fn;  
+ 
+    if(Ma<Mn)
+    { 
+        Ma=Ma+12; 
+        Aa=Aa-1;
+        rM=Ma-Mn; 
+    }
+    else
+        rM=Ma-Mn; 
+    
+ 
+    cout<<"Su edad es: "<<endl;
+    cout<<" Edad: " <<Aa-An<< endl;
+    cout<<" M: "<<rM<<endl; 
+    cout<<" D: "<<rF<<endl;
+    
+   int AA, AN;
+      cout<<"Año actual "<<endl;
+      cin>>Aa;
+     if(Aa % 4 == 0 && (Aa % 100 != 0 || Aa % 400 == 0)){
+         cout<<"El Año "<<Aa<<" Si es bisiesto "<<endl;
+     }else{
+         cout<<"El Año "<<Aa<<" No es bisiesto "<<endl;
+     }
+     
+     cout<<"Año de nacimiento"<<endl;
+     cin>>An;
+        if(An % 4 == 0 && (An % 100 != 0 || An % 400 == 0)){
+         cout<<"El Año "<<An<<" Si es bisiesto "<<endl;
+     }else{
+         cout<<"El Año "<<An<<" No es bisiesto "<<endl;
+     }
+
+
+}
---
+++ 01CONDICIONES/EJEMPLOCONDICIONES.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+//determinar las entradas y salidas del codigo
+using namespace std;
+
+
+main(){
+	//declaracion de variables
+	//programa que nos diga si la persona es mayor de edad
+	int aa, m, d;
+	
+
+	do{
+		cout<<"Ingresa tu fecha de nacimiento";
+		cout<<"Año: ";
+		cin>>aa;
+		cout<<"Mes: ";
+		cin>>m;
+		cout<<"Dia: ";
+		cin>>d
+	
+
+		
+	}
+
+	
+		
+		
+	}
+	
+
