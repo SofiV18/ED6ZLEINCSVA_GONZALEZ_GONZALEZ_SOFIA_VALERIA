51d79326251ad2c15f113c8cb767b3e9672441a0
sv.gonzalez.g@edu.utc.mx <sv.gonzalez.g@edu.utc.mx>
2022-05-25 12:39:01 -0500

    EjemploSentenciaDoWhile

---
+++ SENTENCIAS/EJEMPLOSENTENCIAS.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//uso de sentencias de control
+//do while
+
+int i=1;
+do{
+
+//crear un programa que imprima hasta el 
+cout<<i<<endl;
+i++;
+
+}while(i<11);
+
+}
