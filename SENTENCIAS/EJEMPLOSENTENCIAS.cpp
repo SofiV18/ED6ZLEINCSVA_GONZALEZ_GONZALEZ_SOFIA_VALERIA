dcc863906a456844c3623c89db7bcc85404b2758
sv.gonzalez.g@edu.utc.mx <sv.gonzalez.g@edu.utc.mx>
2022-05-25 13:11:16 -0500

    EjemploWhile

--- SENTENCIAS/EJEMPLOSENTENCIAS.cpp
+++ SENTENCIAS/EJEMPLOSENTENCIAS.cpp
@@ -5,7 +5,7 @@ int main(){
 //uso de sentencias de control
 //do while
 
-int i=1;
+/*int i=1;
 do{
 
 //crear un programa que imprima hasta el 
@@ -13,5 +13,17 @@ cout<<i<<endl;
 i++;
 
 }while(i<11);
+*/
+
+int cont=1;
+while(cont<=10){
+	cout<<cont<<endl;
+	cont++;
+	
+}
+cout<<"se acabo";
+
+
+
 
 }
